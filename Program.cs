74d0d7d7705543bc5d4074e8d5ff2b5efa7e3dce
Annette Hawks <hawksannette@gmail.com>
2025-07-09 21:56:10 -0600

    Maze Rules

--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@
 //Lab 7 Maze 
 //Comment at top of Program.cs giving your name(s), the date, and the name of the lab
 
+
+string mazeRules = "MAZE RULES:\n" +
+    "1. Use the arrow keys to navigate the maze.\n" +
+    "2. The walls are hashtags (#).\n" +
+    "3. You want to find the asterisk * to win.\n";
+
+Console.WriteLine(mazeRules);
+
+
+
